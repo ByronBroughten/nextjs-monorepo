65d40172dbc2618ef06711169898f9ff301cbf0a
Byron Broughten <byron.broughten@gmail.com>
2024-09-15 14:36:28 -0500

    initial commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,14 @@
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(8080, () => {
+  console.log('listening on port 8080')
+})
